ff3e2a56680523a7d687e8e5804700f5625ff6b7
jouzu1 <jouzu1@gmail.com>
2021-05-21 22:28:59 +0700

    post method

--- src/main/java/com/reactnative/controller/UserController.java
+++ src/main/java/com/reactnative/controller/UserController.java
@@ -1,5 +1,37 @@
 package com.reactnative.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reactnative.entity.User;
+import com.reactnative.repository.UserRepository;
+
+@CrossOrigin(origins = "http://localhost:5001")
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	UserRepository userRepo;
+	
+	
+	@GetMapping("")
+	public List<User> getAll() {
+		return (List<User>) this.userRepo.findAll();
+	}
+	
+	
+	@PostMapping("/add")
+//	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String addBuku(@RequestBody User user) {
+		this.userRepo.save(user);
+		return "Insert berhasil";
+	}
 }
